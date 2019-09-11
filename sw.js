d6e1f2be589b8de71ecdf169d05628455c2c0322
Vincent van Bergen <v.vanbergen@gmail.com>
2019-09-11 17:04:39 +0200

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-56bcb0a7fa69bc647524.js"
+    "url": "webpack-runtime-6810912a4abfb0a5e80e.js"
   },
   {
     "url": "app-bdc1a0ec7737adeabb36.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1510ecd7a5967181e59cb5287d998e7d"
+    "revision": "b14d65012f32295dcb1c9fc727bbc057"
   },
   {
     "url": "manifest.json",
